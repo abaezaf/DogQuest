1e3f28b998e531e94fcd18ccd4d8fee6b34a258c
abaezaf <antonio.baezafdez@gmail.com>
2021-01-20 07:43:05 +0100

    Random objects tiran, falta controlarlos mejor

--- DogQuest/entities.py
+++ DogQuest/entities.py
@@ -7,6 +7,11 @@ import sys
 
 pg.init()
 
+screen = pg.display.set_mode(GAME_DIMENSIONS)
+pg.display.set_caption("DogQuest")
+
+obstacles = []
+
 class Dog:
     def __init__(self, x, y, vy):
         self.x = x
@@ -32,15 +37,61 @@ class Dog:
                 self.vy = 0
         else:
             self.vy = 0
+        
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
 
-class Obstacle:
-    def __init__(self, x, y, vx):
+class Rock:
+    def __init__(self, x, y, vx, width, height):
         self.x = x
         self.y = y
         self.vx = vx
+        self.width = width
+        self.height = height
+        self.hitbox = (x, y, width, height)
 
         self.image = pg.image.load("Resources/Images/rock.png").convert_alpha()
         self.rect = self.image.get_rect()
+
+    def movement(self):
+        if self.x >= GAME_DIMENSIONS[0]:
+            self.vx = 0 
+        else:
+            self.x -= self.vx   
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
+class Hole:
+    def __init__(self, x, y, vx, width, height):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.width = width
+        self.height = height
+        self.hitbox = (x, y, width, height)
+
+        self.image = pg.image.load("Resources/Images/hole.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))        
+
+class Cat:
+    def __init__(self, x, y, vx, width, height):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.width = width
+        self.height = height
+        self.hitbox = (x, y, width, height)
+
+        self.image = pg.image.load("Resources/Images/cat.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+
 
 class Background:
     def __init__(self):
@@ -51,14 +102,12 @@ class Background:
 
 class Game:
     def __init__(self):
-        self.screen = pg.display.set_mode(GAME_DIMENSIONS)
-        pg.display.set_caption("DogQuest")
-
         self.dog = Dog(50, 250, 0)
-        self.obstacle = Obstacle(400, 150, 20)
+
         self.background = Background()
 
         self.clock = pg.time.Clock()
+
 
 
     def main_loop(self):
@@ -77,8 +126,26 @@ class Game:
             self.dog.control()
             self.dog.update()
 
-            self.screen.blit(self.background.image, (0, 0))
-            self.screen.blit(self.dog.image, (self.dog.x, self.dog.y))
-            self.screen.blit(self.obstacle.image, (self.obstacle.x, self.obstacle.y))
+
+            screen.blit(self.background.image, (0, 0))
+            self.dog.draw(screen)
+
+            for obstacle in obstacles:
+                obstacle.draw(screen)
+                obstacle.x -= 1.4
+                if obstacle.x < obstacle.width * -1:
+                    obstacles.pop(obstacles.index(obstacle))
+
+            pg.time.set_timer(USEREVENT+2, random.randrange(2000, 3500))
+            if event.type == USEREVENT+2:
+                r = random.randrange(0,100)
+                if r == 0:
+                    obstacles.append(Rock(750, random.randint(1, 550), 1, 50, 50))
+                elif r == 1:
+                    obstacles.append(Hole(750, random.randint(1, 550), 1, 50, 50))
+                elif r == 2:
+                    obstacles.append(Cat(750, random.randint(1, 550), 1, 50, 50))
+                    
+            
 
             pg.display.flip()
